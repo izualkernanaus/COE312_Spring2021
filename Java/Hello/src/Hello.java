f0ff4d2d15ce0226f83513ef960f8c129782b734
izualkernan <izualkernan@aus.edu>
2021-02-08 13:23:06 +0400

    Feb 8, 2021

--- Java/Hello/src/Hello.java
+++ Java/Hello/src/Hello.java
@@ -1,10 +1,54 @@
 
+// just like C++ we have a class name
+// neme is hello
+
 public class Hello {
 
+	// the main function is INSIDE one class
+	// in C++ main was floating around
+	// One program running can only have one main just like C++
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Hello");
+		
+	// cout << "hello world!"
+		
+	   System.out.println("hello world from java");
+	
+	 // int is just like C++
+	   
+	   int x = 10;
+	   
+	   // cout << x;
+	   System.out.println(x);
+	   
+	  // Integer is ?
+	  
+	  // -- Integer is a dog
+	  //    -- Integer is the same as if we said int *
+	  // -- Integer is the same as int
+	   
+	   
+	   
+	   // Java does not have pointers == Almost everything in Java
+	   // is a pointer 
+	   // "because everthing is the same as nothing"
+	   
+	   // int * y;  -- y is an address of an Integer class
+	   //           -- y is a pointer to an Integer class
+	   
+	   
+	   Integer y;
+	   
+	   // *y = 20
+	   
+	   // dog on the left side -- number on the right side
+	   
+	   y = 20;
+	   
+	   y = new Integer(30);
+	   
+	   System.out.println(y);
+	   
 	}
 
 }
